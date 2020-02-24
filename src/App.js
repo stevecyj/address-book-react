eb8b9cae1f09348c188c09aca7d00ebce9259f62
Steve <ycsteven.tsao@gmail.com>
2020-02-24 17:00:28 +0800

    to step ContactForm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
+import { createBrowserHistory as createHistory } from 'history';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
-
+const history = createHistory();
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router history={history}>
+                <Navbar bg="primary" expand="lg" variant="dark">
+                    <Navbar.Brand href="#home">Address Book App</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Route path="/" exact component={HomePage} />
+            </Router>
+        </div>
+    );
 }
-
 export default App;
